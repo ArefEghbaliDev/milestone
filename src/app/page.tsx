0618a925d37653e505cb3805d6690bd12435e29f
ArefEghbaliDev <arefeghbalidev@outlook.com>
2023-07-14 21:47:16 +0330

    chore: Init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className={inter.className}></main>;
+    return <main className={inter.className}></main>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    700: '#0052CC',
+                    500: '#005CE6',
+                    100: '#85B6FF',
+                },
+                secondary: {
+                    700: '#D19FA7',
+                    500: '#DEBAC0',
+                    100: '#E4C8CD',
+                },
+                dark: {
+                    500: '#0D0221',
+                },
+                powder: {
+                    500: '#FFFDF7',
+                },
+            },
+        },
+        borderRadius: {
+            DEFAULT: '8px',
+            FULL: '99px',
+            lg: '12px',
+            sm: '4px',
+        },
+    },
+    plugins: [],
 };
