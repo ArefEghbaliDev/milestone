23be1a9bcbe4b7ecd99071dc3e4d330d40b490b1
ArefEghbaliDev <arefeghbalidev@outlook.com>
2023-07-22 21:36:43 +0330

    feat: Add project item

---
+++ src/components/ui/molecules/project-item.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { Project } from 'src/models/project.model';
+
+type ProjectItemProps = {
+    project: Project;
+};
+
+export default function ProjectItem({ project }: ProjectItemProps) {
+    return (
+        <Link
+            href={`/projects/p/${project.project_key}`}
+            className="rounded p-5 border border-dark-100 transition-colors duration-150 hover:bg-dark-100"
+        >
+            <p className="font-medium">{project.title}</p>
+            <p className="text-sm">{project.project_key}</p>
+        </Link>
+    );
+}
