a419be75eb21859536aa7cf22e8cf54799cac2f3
ArefEghbaliDev <arefeghbalidev@outlook.com>
2023-07-29 13:26:04 +0330

    refactor: Changed to Prospire

--- src/app/api/v1/organization/[oid]/route.ts
+++ src/app/api/v1/organization/[oid]/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest,NextResponse } from "next/server";
 import { Project } from "src/models/project.model";
 
-export const GET = async (request: NextRequest,{ params }: { params: { pid: string } }): Promise<NextResponse> => {
+export const GET = async (request: NextRequest,{ params }: { params: { oid: string } }): Promise<NextResponse> => {
     const { userId } = getAuth(request);
 
 
@@ -16,7 +16,7 @@ export const GET = async (request: NextRequest,{ params }: { params: { pid: stri
 
     const prisma = new PrismaClient();
 
-    const project = await prisma.project.findFirst({ where: { project_key: params.pid,creator_id: userId } });
+    const project = await prisma.project.findFirst({ where: { project_key: params.oid,creator_id: userId } });
 
     if (!project) {
         return NextResponse.json({
